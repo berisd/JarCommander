c6bf002a40dd0beb3247dcd81c252400707256c3
Bernd Riedl <b.riedl@beris.at>
2015-10-10 23:18:48 +0200

    Delete selection

--- src/main/java/at/beris/jaxcommander/action/DeleteAction.java
+++ src/main/java/at/beris/jaxcommander/action/DeleteAction.java
@@ -9,12 +9,19 @@
 
 package at.beris.jaxcommander.action;
 
+import at.beris.jaxcommander.Application;
+import at.beris.jaxcommander.ui.NavigationPanel;
+import at.beris.jaxcommander.ui.SessionPanel;
 import org.apache.log4j.Logger;
 
 import javax.swing.Action;
+import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
 
 public class DeleteAction extends CustomAction {
     private final static Logger LOGGER = Logger.getLogger(DeleteAction.class);
@@ -33,8 +40,24 @@ public class DeleteAction extends CustomAction {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent event) {
         LOGGER.info("Delete");
+
+        Application application = (Application) SwingUtilities.getRoot((Component) event.getSource());
+        SessionPanel sessionPanel = application.getSessionPanel();
+        NavigationPanel sourcePanel = sessionPanel.getSelectedNavigationPanel();
+
+        if (sourcePanel.getSelection().size() == 0) {
+            JOptionPane.showMessageDialog(application, "Nothing selected!");
+        } else {
+            int deletion = JOptionPane.showConfirmDialog(application, "Delete current selection?", "Deletion", JOptionPane.YES_NO_OPTION);
+            if (deletion == JOptionPane.YES_OPTION) {
+                for (File file : sourcePanel.getSelection()) {
+                    file.delete();
+                }
+                sourcePanel.refreshDirectory();
+            }
+        }
     }
 
     @Override
