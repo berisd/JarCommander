d6960cd3619a90a5da644f5f96108bf736535be9
Bernd Riedl <bernd.riedl@gmail.com>
2016-01-30 22:46:55 +0100

    Rework renderer for File Attribute Column

--- src/main/java/at/beris/jarcommander/ui/table/FileAttributesRenderer.java
+++ src/main/java/at/beris/jarcommander/ui/table/FileAttributesRenderer.java
@@ -10,64 +10,54 @@
 package at.beris.jarcommander.ui.table;
 
 import at.beris.virtualfile.Attribute;
-import at.beris.virtualfile.UnixAttribute;
-import at.beris.virtualfile.WindowsAttribute;
 
 import javax.swing.*;
 import javax.swing.table.TableCellRenderer;
 import java.awt.*;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 
 import static at.beris.jarcommander.ApplicationContext.SELECTION_FOREGROUND_COLOR;
 
 public class FileAttributesRenderer extends JLabel implements TableCellRenderer {
+    private Map<Attribute, AttributeText> attributeTextMap;
+    private List<Attribute> windowsAttributeList;
+    private List<Attribute> posixAttributeList;
+
+    public FileAttributesRenderer() {
+        attributeTextMap = createAttributeTextMap();
+
+        windowsAttributeList = Arrays.asList(Attribute.READ_ONLY, Attribute.WRITE, Attribute.EXECUTE);
+        posixAttributeList = Arrays.asList(Attribute.OWNER_READ, Attribute.OWNER_WRITE,
+                Attribute.OWNER_EXECUTE, Attribute.GROUP_READ, Attribute.GROUP_WRITE, Attribute.GROUP_EXECUTE,
+                Attribute.OTHERS_READ, Attribute.OTHERS_WRITE, Attribute.OTHERS_EXECUTE);
+    }
+
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-
         StringBuilder shortText = new StringBuilder("");
         StringBuilder longText = new StringBuilder("");
-
         Set<Attribute> fileAttributes = (Set<Attribute>) value;
 
-        Attribute firstFileAttribute = null;
-        if (fileAttributes != null && fileAttributes.iterator().hasNext()) {
-            firstFileAttribute = fileAttributes.iterator().next();
-        }
+        boolean isWindows = fileAttributes.contains(Attribute.READ_ONLY) || fileAttributes.contains(Attribute.WRITE) || fileAttributes.contains(Attribute.EXECUTE);
+        List<Attribute> attributeList = isWindows ? windowsAttributeList : posixAttributeList;
 
-        if (firstFileAttribute != null) {
-            List<Attribute> attributes;
-            if (firstFileAttribute instanceof UnixAttribute)
-                attributes = Arrays.asList((Attribute[]) UnixAttribute.values());
-            else
-                attributes = Arrays.asList((Attribute[]) WindowsAttribute.values());
-
-            for (Attribute attribute : attributes) {
-                if (fileAttributes.contains(attribute)) {
-                    if (attribute == UnixAttribute.S_IXUSR && fileAttributes.contains(UnixAttribute.S_ISUID))
-                        continue;
-                    if (attribute == UnixAttribute.S_IXGRP && fileAttributes.contains(UnixAttribute.S_ISGID))
-                        continue;
-                    shortText.append(attribute.shortName());
-                    if (longText.length() > 0)
-                        longText.append("<br>");
-                    longText.append(attribute.longName());
-                } else if (attribute != WindowsAttribute.HIDDEN && attribute != UnixAttribute.S_ISUID && attribute != UnixAttribute.S_ISGID) {
-                    shortText.append('-');
-                    longText.append('-');
-                }
+        for (Attribute attribute : attributeList) {
+            if (fileAttributes.contains(attribute)) {
+                shortText.append(attributeTextMap.get(attribute).shortText);
+                if (longText.length() > 0)
+                    longText.append("<br>");
+                longText.append(attributeTextMap.get(attribute).longText);
+            } else {
+                shortText.append('-');
             }
-
-            longText.insert(0, "<html>");
-            longText.append("</html>");
-
-            setText(shortText.toString());
-            setToolTipText(longText.toString());
-        } else {
-            setText("");
-            setToolTipText("");
         }
+
+        longText.insert(0, "<html>");
+        longText.append("</html>");
+
+        setText(shortText.toString());
+        setToolTipText(longText.toString());
 
         if (isSelected) {
             setForeground(SELECTION_FOREGROUND_COLOR);
@@ -76,5 +66,35 @@ public class FileAttributesRenderer extends JLabel implements TableCellRenderer 
         }
 
         return this;
+    }
+
+    private class AttributeText {
+        public AttributeText(char shortText, String longText) {
+            this.shortText = shortText;
+            this.longText = longText;
+        }
+
+        public char shortText;
+        public String longText;
+    }
+
+    private Map<Attribute, AttributeText> createAttributeTextMap() {
+        Map<Attribute, AttributeText> attributeTextMap = new HashMap<>();
+        attributeTextMap.put(Attribute.READ_ONLY, new AttributeText('R', "Read"));
+        attributeTextMap.put(Attribute.WRITE, new AttributeText('W', "Write"));
+        attributeTextMap.put(Attribute.EXECUTE, new AttributeText('X', "Execute"));
+        attributeTextMap.put(Attribute.HIDDEN, new AttributeText('H', "Hidden"));
+        attributeTextMap.put(Attribute.ARCHIVE, new AttributeText('A', "Archive"));
+        attributeTextMap.put(Attribute.OWNER_READ, new AttributeText('R', "Owner Read"));
+        attributeTextMap.put(Attribute.OWNER_WRITE, new AttributeText('W', "Owner Write"));
+        attributeTextMap.put(Attribute.OWNER_EXECUTE, new AttributeText('X', "Owner Execute"));
+        attributeTextMap.put(Attribute.GROUP_READ, new AttributeText('R', "Group Read"));
+        attributeTextMap.put(Attribute.GROUP_WRITE, new AttributeText('W', "Group Write"));
+        attributeTextMap.put(Attribute.GROUP_EXECUTE, new AttributeText('X', "Group Execute"));
+        attributeTextMap.put(Attribute.OTHERS_READ, new AttributeText('R', "Others Read"));
+        attributeTextMap.put(Attribute.OTHERS_WRITE, new AttributeText('W', "Others Write"));
+        attributeTextMap.put(Attribute.OTHERS_EXECUTE, new AttributeText('X', "Others Execute"));
+
+        return attributeTextMap;
     }
 }
